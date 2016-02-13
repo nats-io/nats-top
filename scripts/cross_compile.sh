449e6b5b4a3dabd793f4dba6c196c880122fc4fc
Waldemar Quevedo <wally@apcera.com>
2016-02-13 14:50:49 -0800

    Cross compile checks

--- scripts/cross_compile.sh
+++ scripts/cross_compile.sh
@@ -3,9 +3,25 @@ go get github.com/mitchellh/gox
 go get github.com/tcnksm/ghr
 
 export APPNAME="nats-top"
-export OSARCH="linux/amd64 darwin/amd64 linux/arm solaris/amd64 windows/amd64"
-export DIRS="linux_amd64 darwin_amd64 linux_arm solaris_amd64 windows_amd64"
+export OSARCH="linux/amd64 darwin/amd64 linux/arm windows/amd64"
+export DIRS="linux_amd64 darwin_amd64 linux_arm windows_amd64"
 export OUTDIR="pkg"
+
+rm ../gnatsd/server/pse_*.go
+cat <<HERE > ../gnatsd/server/pse.go
+// Copyright 2015 Apcera Inc. All rights reserved.
+
+package server
+
+// This is a placeholder for now.
+func procUsage(pcpu *float64, rss, vss *int64) error {
+	*pcpu = 0.0
+	*rss = 0
+	*vss = 0
+
+	return nil
+}
+HERE
 
 gox -osarch="$OSARCH" -output "$OUTDIR/$APPNAME-{{.OS}}_{{.Arch}}/$APPNAME"
 for dir in $DIRS; do \
