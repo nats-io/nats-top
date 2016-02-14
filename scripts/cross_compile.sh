2e0d89af12e2105ff30e558197c6107a79560979
Waldemar Quevedo <wally@apcera.com>
2016-02-13 17:29:43 -0800

    Add cleanup to build script

--- scripts/cross_compile.sh
+++ scripts/cross_compile.sh
@@ -28,7 +28,8 @@ for dir in $DIRS; do \
 	(cp readme.md $OUTDIR/$APPNAME-$dir/readme.md) ;\
 	(cp LICENSE $OUTDIR/$APPNAME-$dir/LICENSE) ;\
 	(cd $OUTDIR && zip -q $APPNAME-$dir.zip -r $APPNAME-$dir) ;\
-	echo "make $OUTDIR/$APPNAME-$dir.zip" ;\
+	echo "created '$OUTDIR/$APPNAME-$dir.zip', cleaning up..." ;\
+	rm -rf $APPNAME-$dir;\
 done
 
 pwd
