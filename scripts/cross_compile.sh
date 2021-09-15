a8b24aab190315e3582c6876a21bf73284d8773c
Phil Pennock <pdp@synadia.com>
2021-09-14 21:05:12 -0400

    Build with current Go tooling, more cleanly

--- scripts/cross_compile.sh
+++ scripts/cross_compile.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
-go get github.com/mitchellh/gox
-go get github.com/tcnksm/ghr
+go install github.com/mitchellh/gox@latest
+go install github.com/tcnksm/ghr@latest
 
-export APPNAME="nats-top"
-export OSARCH="linux/amd64 darwin/amd64 linux/arm windows/amd64"
-export DIRS="linux_amd64 darwin_amd64 linux_arm windows_amd64"
-export OUTDIR="pkg"
-
-# NOTE: Hack for avoiding issues when cross compiling.
-rm $HOME/gopath/src/github.com/nats-io/gnatsd/server/pse_*.go
-cat <<HERE > $HOME/gopath/src/github.com/nats-io/gnatsd/server/pse.go
-// Copyright 2016 Apcera Inc. All rights reserved.
-package server
-// This is a placeholder for now.
-func procUsage(pcpu *float64, rss, vss *int64) error {
-	*pcpu = 0.0
-	*rss = 0
-	*vss = 0
-	return nil
-}
-HERE
+APPNAME="nats-top"
+OSARCH="linux/amd64 darwin/amd64 linux/arm windows/amd64"
+DIRS="linux_amd64 darwin_amd64 linux_arm windows_amd64"
+OUTDIR="pkg"
 
 gox -osarch="$OSARCH" -output "$OUTDIR/$APPNAME-{{.OS}}_{{.Arch}}/$APPNAME"
 for dir in $DIRS; do \
